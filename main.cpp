f3c35286df8ddeed1d29b87765c90374adf9cae6
abel <abel@abellee.cn>
2022-07-19 21:28:33 +0800

    delete retrieval modules

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <grpcpp/server_builder.h>
 
-#include "./service/RepositoryService.h"
 #include "./service/DetectService.h"
 #include "./service/RecognitionService.h"
 
@@ -11,13 +10,11 @@ int main(int argc, char *argv[]) {
     std::cout << "Starting All Servers!" << std::endl;
 
     std::string server_address("0.0.0.0:50001");
-    RepositoryService repositoryService;
     DetectService detectService;
     RecognitionService recognitionService;
 
     grpc::ServerBuilder builder;
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&repositoryService);
     builder.RegisterService(&detectService);
     builder.RegisterService(&recognitionService);
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
